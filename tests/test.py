15504b47780fb613b8563588fe328f0dcfd97dfa
chos9 <chos9@uw.edu>
2022-12-20 23:10:09 +0900

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,25 +1,52 @@
-# from pykrx import stock
-# import pandas as pd
+## KRX 전종목 시세 데이터 크롤링
+import requests
+import pandas as pd
+from io import StringIO ## for Python 3
+import io
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+import exchange_calendars as ecals
 
-# df = stock.get_market_trading_volume_by_investor("20210115", "20210122", "005930")
-# print(df.head())
 
-# # low_list = [ ]
-# # tickers = stock.get_market_ticker_list()
-# # print(tickers)
-# # for ticker in tickers:
-# #     df = stock.get_market_ohlcv_by_date("20210103", "20210430", ticker)
-# #     low_list.append(df['저가'])
+XKRX = ecals.get_calendar("XKRX")
+dates = []
+for i in pd.date_range('1/1/2017', '12/1/2022', freq='MS'):
+    temp = i
+    while XKRX.is_session(temp.strftime('%Y-%m-%d')) == False:
+        temp = temp + timedelta(days=1)
+    dates.append(temp)
 
-# # df = pd.concat(low_list, axis=1)
-# # print(df)
-import pykrx
-print(pykrx.__version__)
 
-from pykrx import stock
+gen_otp_url = 'http://data.krx.co.kr/comm/fileDn/GenerateOTP/generate.cmd'
+down_url = 'http://data.krx.co.kr/comm/fileDn/download_csv/download.cmd'
+referer = {'referer' : gen_otp_url}
 
-# df = stock.get_market_price_change_by_ticker(fromdate="20210104", todate="20210111")
-# print(df)
+df = pd.DataFrame(columns=['거래일자','종목코드','종목명','시장구분','소속부','종가','대비','등락률','시가','고가','저가','거래량','거래대금','시가총액','상장주식수'])
 
-print(stock.get_market_net_purchases_of_equities_by_ticker('20210801', '20210831', 'ALL', '기관합계'))
+for i in dates:
+    gen_otp_data = {
+        'locale' : 'ko_KR',
+        'mktId' : 'ALL',
+        'trdDd' : i.strftime('%Y%m%d'),
+        'share' : '1',
+        'money' : '1',
+        'csvxls_isNo' : 'false',
+        'name' : 'fileDown',
+        'url' : 'dbms/MDC/STAT/standard/MDCSTAT01501',
+    }
+    otp = requests.post(gen_otp_url, params = gen_otp_data)
+    otp = BeautifulSoup(otp.content, "html.parser").text
+    otp_key = {"code" : otp}
+    down = requests.post(down_url, data= otp_key, headers = referer).content.decode('EUC-KR')
+    down = StringIO(down)
+    df_temp = pd.read_csv(down, sep = ',')
+
+    df_temp['거래일자'] = i.strftime('%Y%m%d')
+    df = pd.concat([df, df_temp])
+
+file_dir = "./"
+file_name = "data.csv"
+df.to_csv(file_dir + file_name,index=False, index_label=None)
+# print('Job Finished')
+# print(file_dir + file_name + ' Saved')
 
